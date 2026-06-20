b6f49ba55cfce18c4e7c4493fa5f30a3c2953861
YairInbar-coder <yair.inbar.mail@gmail.com>
2026-06-20 14:48:50 +0300

    build.yml fix

--- src/App.h
+++ src/App.h
@@ -3,8 +3,7 @@
 
 #pragma once
 #include <SDL3/SDL.h>
-
-
+#include <SDL3_ttf/SDL_ttf.h>
 
 // main app stuff
 
@@ -12,9 +11,11 @@ SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 SDL_Event event;
 bool running = true;
+TTF_Font* font = TTF_OpenFont()
 
 // first frame
 void Start() {
+	TTF_Init()
 	window = SDL_CreateWindow("FeatherCode", 800, 600, SDL_WINDOW_RESIZABLE);
 	renderer = SDL_CreateRenderer(window, "opengl");	
 }
